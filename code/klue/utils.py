345f36f13615a6a760d2f1ed9e70e38d91429cfd
soypabloo <kate49143@gmail.com>
2022-11-14 13:14:32 +0000

    Formatting using black and isort

--- code/klue/utils.py
+++ code/klue/utils.py
@@ -1,14 +1,14 @@
-import os
 import math
-import torch
+import os
+import pickle as pickle
 import random
 
 import numpy as np
 import pandas as pd
-import pickle as pickle
+import torch
 
 
-def set_seed(random_seed):
+def set_seed(random_seed: int) -> None:
     os.environ["PYTHONHASHSEED"] = str(random_seed)
     random.seed(random_seed)
     torch.manual_seed(random_seed)
@@ -17,24 +17,26 @@ def set_seed(random_seed):
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
     np.random.seed(random_seed)
-    
-def label_to_num(label):
-  num_label = []
-  with open('dict_label_to_num.pkl', 'rb') as f:
-    dict_label_to_num = pickle.load(f)
-  for v in label:
-    num_label.append(dict_label_to_num[v])
-  
-  return num_label
+
+
+def label_to_num(label) -> list:
+    num_label = []
+    with open("dict_label_to_num.pkl", "rb") as f:
+        dict_label_to_num = pickle.load(f)
+    for v in label:
+        num_label.append(dict_label_to_num[v])
+
+    return num_label
+
 
 def num_to_label(label):
-  """
+    """
     숫자로 되어 있던 class를 원본 문자열 라벨로 변환 합니다.
-  """
-  origin_label = []
-  with open('dict_num_to_label.pkl', 'rb') as f:
-    dict_num_to_label = pickle.load(f)
-  for v in label:
-    origin_label.append(dict_num_to_label[v])
-  
-  return origin_label
+    """
+    origin_label = []
+    with open("dict_num_to_label.pkl", "rb") as f:
+        dict_num_to_label = pickle.load(f)
+    for v in label:
+        origin_label.append(dict_num_to_label[v])
+
+    return origin_label
--- code/train.py
+++ code/train.py
@@ -1,90 +1,98 @@
-import torch
 import pickle as pickle
 
+import torch
 from args import get_args
-from klue.utils import set_seed, label_to_num
-from klue.metric import klue_re_auprc, klue_re_micro_f1, compute_metrics
-from klue.dataloader import RE_Dataset, tokenized_dataset, load_data, preprocessing_dataset
+from klue.dataloader import (RE_Dataset, load_data, preprocessing_dataset,
+                             tokenized_dataset)
+from klue.metric import compute_metrics, klue_re_auprc, klue_re_micro_f1
+from klue.utils import label_to_num, set_seed
+from transformers import (AutoConfig, AutoModelForSequenceClassification,
+                          AutoTokenizer, BertTokenizer, RobertaConfig,
+                          RobertaForSequenceClassification, RobertaTokenizer,
+                          Trainer, TrainingArguments)
 
-from transformers import AutoTokenizer, AutoConfig, AutoModelForSequenceClassification, Trainer, TrainingArguments, RobertaConfig, RobertaTokenizer, RobertaForSequenceClassification, BertTokenizer
 
 def train():
-  device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-  
-  # load model and tokenizer
-  # TODO: BETTER WAY TO SET DIRECTORIES!!!!
-  MODEL_NAME = f"{args.model_name.replace('/','_')}_{args.version}"
-  SAVE_DIR = f"{args.save_dir}/{MODEL_NAME}"
-  LOG_DIR = f"{args.logs_dir}/{MODEL_NAME}"
-  MODEL_DIR = f"{args.model_dir}/{MODEL_NAME}"
-  
-  tokenizer = AutoTokenizer.from_pretrained(args.model_name)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-  # load dataset
-  train_dataset = load_data(args.train_path)
-  # dev_dataset = load_data("../dataset/train/dev.csv") # validation용 데이터는 따로 만드셔야 합니다.
+    # load model and tokenizer
+    # TODO: BETTER WAY TO SET DIRECTORIES!!!!
+    MODEL_NAME = f"{args.model_name.replace('/','_')}_{args.version}"
+    SAVE_DIR = f"{args.save_dir}/{MODEL_NAME}"
+    LOG_DIR = f"{args.logs_dir}/{MODEL_NAME}"
+    MODEL_DIR = f"{args.model_dir}/{MODEL_NAME}"
 
-  train_label = label_to_num(train_dataset['label'].values)
-  # dev_label = label_to_num(dev_dataset['label'].values)
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name)
 
-  # tokenizing dataset
-  tokenized_train = tokenized_dataset(train_dataset, tokenizer)
-  # tokenized_dev = tokenized_dataset(dev_dataset, tokenizer)
+    # load dataset
+    train_dataset = load_data(args.train_path)
+    # dev_dataset = load_data("../dataset/train/dev.csv") # validation용 데이터는 따로 만드셔야 합니다.
 
-  # make dataset for pytorch.
-  train_dataset = RE_Dataset(tokenized_train, train_label)
-  # RE_dev_dataset = RE_Dataset(tokenized_dev, dev_label)
+    train_label = label_to_num(train_dataset["label"].values)
+    # dev_label = label_to_num(dev_dataset['label'].values)
+
+    # tokenizing dataset
+    tokenized_train = tokenized_dataset(train_dataset, tokenizer)
+    # tokenized_dev = tokenized_dataset(dev_dataset, tokenizer)
+
+    # make dataset for pytorch.
+    train_dataset = RE_Dataset(tokenized_train, train_label)
+    # RE_dev_dataset = RE_Dataset(tokenized_dev, dev_label)
+
+    print(device)
+    # setting model hyperparameter
+    model_config = AutoConfig.from_pretrained(args.model_name)
+    model_config.num_labels = 30
+
+    model = AutoModelForSequenceClassification.from_pretrained(
+        args.model_name, config=model_config
+    )
+    print(model.config)
+    model.parameters
+    model.to(device)
+
+    # 사용한 option 외에도 다양한 option들이 있습니다.
+    # https://huggingface.co/transformers/main_classes/trainer.html#trainingarguments 참고해주세요.
+    training_args = TrainingArguments(
+        output_dir=SAVE_DIR,  # output directory
+        save_total_limit=5,  # number of total save model.
+        save_steps=500,  # model saving step.
+        num_train_epochs=args.max_epoch,  # total number of training epochs
+        learning_rate=args.learning_rate,  # learning_rate
+        per_device_train_batch_size=args.batch_size,  # batch size per device during training
+        per_device_eval_batch_size=args.batch_size,  # batch size for evaluation
+        warmup_steps=500,  # number of warmup steps for learning rate scheduler
+        weight_decay=0.01,  # strength of weight decay
+        logging_dir=LOG_DIR,  # directory for storing logs
+        logging_steps=100,  # log saving step.
+        evaluation_strategy=args.eval_strategy,  # evaluation strategy to adopt during training
+        # `no`: No evaluation during training.
+        # `steps`: Evaluate every `eval_steps`.
+        # `epoch`: Evaluate every end of epoch.
+        eval_steps=500,  # evaluation step.
+        seed=args.seed,
+        fp16=True
+        # TODO: evaluation_strategy 에 맞춰 변경이 필요함.
+        # load_best_model_at_end = True
+    )
+    trainer = Trainer(
+        model=model,  # the instantiated 🤗 Transformers model to be trained
+        args=training_args,  # training arguments, defined above
+        train_dataset=train_dataset,  # training dataset
+        eval_dataset=train_dataset,  # evaluation dataset
+        compute_metrics=compute_metrics,  # define metrics function
+    )
+
+    # train model
+    trainer.train()
+    model.save_pretrained(MODEL_DIR)
 
 
-  print(device)
-  # setting model hyperparameter
-  model_config =  AutoConfig.from_pretrained(args.model_name)
-  model_config.num_labels = 30
-
-  model =  AutoModelForSequenceClassification.from_pretrained(args.model_name, config=model_config)
-  print(model.config)
-  model.parameters
-  model.to(device)
-  
-  # 사용한 option 외에도 다양한 option들이 있습니다.
-  # https://huggingface.co/transformers/main_classes/trainer.html#trainingarguments 참고해주세요.
-  training_args = TrainingArguments(
-    output_dir=SAVE_DIR,          # output directory
-    save_total_limit=5,              # number of total save model.
-    save_steps=500,                 # model saving step.
-    num_train_epochs=args.max_epoch,              # total number of training epochs
-    learning_rate=args.learning_rate,               # learning_rate
-    per_device_train_batch_size=args.batch_size,  # batch size per device during training
-    per_device_eval_batch_size=args.batch_size,   # batch size for evaluation
-    warmup_steps=500,                # number of warmup steps for learning rate scheduler
-    weight_decay=0.01,               # strength of weight decay
-    logging_dir=LOG_DIR,            # directory for storing logs
-    logging_steps=100,              # log saving step.
-    evaluation_strategy=args.eval_strategy, # evaluation strategy to adopt during training
-                                # `no`: No evaluation during training.
-                                # `steps`: Evaluate every `eval_steps`.
-                                # `epoch`: Evaluate every end of epoch.
-    eval_steps = 500,            # evaluation step.
-    seed=args.seed,
-    fp16=True
-    # TODO: evaluation_strategy 에 맞춰 변경이 필요함.
-    # load_best_model_at_end = True
-  )
-  trainer = Trainer(
-    model=model,                         # the instantiated 🤗 Transformers model to be trained
-    args=training_args,                  # training arguments, defined above
-    train_dataset=train_dataset,         # training dataset
-    eval_dataset=train_dataset,             # evaluation dataset
-    compute_metrics=compute_metrics         # define metrics function
-  )
-
-  # train model
-  trainer.train()
-  model.save_pretrained(MODEL_DIR)
 def main():
-  set_seed(args.seed)
-  train()
+    set_seed(args.seed)
+    train()
 
-if __name__ == '__main__':
-  args = get_args(mode='train')
-  main()
+
+if __name__ == "__main__":
+    args = get_args(mode="train")
+    main()
