5fa73f90548ab4f4daff0d1a9c915acebd2fe813
Sangsu <ggb04110@naver.com>
2022-11-15 11:39:28 +0000

    fix utils.py Absolute Path

--- code/klue/utils.py
+++ code/klue/utils.py
@@ -21,7 +21,7 @@ def set_seed(random_seed: int) -> None:
 
 def label_to_num(label) -> list:
     num_label = []
-    with open("../../code/dict_label_to_num.pkl", "rb") as f:
+    with open("/opt/ml/code/dict_label_to_num.pkl", "rb") as f:
         dict_label_to_num = pickle.load(f)
     for v in label:
         num_label.append(dict_label_to_num[v])
@@ -34,7 +34,7 @@ def num_to_label(label):
     숫자로 되어 있던 class를 원본 문자열 라벨로 변환 합니다.
     """
     origin_label = []
-    with open("../../code/dict_num_to_label.pkl", "rb") as f:
+    with open("/opt/ml/code/dict_num_to_label.pkl", "rb") as f:
         dict_num_to_label = pickle.load(f)
     for v in label:
         origin_label.append(dict_num_to_label[v])
