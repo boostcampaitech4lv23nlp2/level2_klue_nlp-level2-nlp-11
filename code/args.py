639cedefc51f58f790ca3522afda56282690763e
Sangsu <ggb04110@naver.com>
2022-11-15 11:33:44 +0000

    fix format black

--- code/args.py
+++ code/args.py
@@ -3,14 +3,14 @@ import sys
 
 from omegaconf import OmegaConf
 
+
 def get_args(mode="train"):
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--config", "-c", type=str, default="base_config")
-
     args, _ = parser.parse_known_args()
 
     # pip install omegaconf 부터
     conf = OmegaConf.load(f"../config/{args.config}.yaml")
 
-    return args , conf
+    return args, conf
