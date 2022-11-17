d73266b6e2936d1968359b2f8f972f7a92c28945
soypabloo <kate49143@gmail.com>
2022-11-17 15:37:04 +0000

    🐛 delete debug logging "print()"

--- code/klue/utils.py
+++ code/klue/utils.py
@@ -65,10 +65,8 @@ class FocalLoss(torch.nn.Module):
             input = input.transpose(1, 2)  # N,C,H*W => N,H*W,C
             input = input.contiguous().view(-1, input.size(2))  # N,H*W,C => N*H*W,C
         target = target.view(-1, 1)
-        print(target.shape)
 
         logpt = F.log_softmax(input, dim=-1)
-        print(logpt.shape)
         logpt = torch.gather(logpt, 1, target)
         logpt = logpt.view(-1)
         pt = Variable(logpt.data.exp())
