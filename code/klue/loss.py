838dc368c97602974e4fe4aba27c414c952a8308
soypabloo <kate49143@gmail.com>
2022-11-19 13:10:39 +0000

    🏷️update defalut gamma=2

--- code/klue/loss.py
+++ code/klue/loss.py
@@ -4,7 +4,7 @@ from torch.autograd import Variable
 
 
 class FocalLoss(torch.nn.Module):
-    def __init__(self, gamma=0, alpha=None, size_average=True):
+    def __init__(self, gamma=2, alpha=None, size_average=True):
         super(FocalLoss, self).__init__()
         self.gamma = gamma
         self.alpha = alpha
