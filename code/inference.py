4bc8fb8e3b29d59b5cd87b8eae800e4197cc34c2
Sangsu <ggb04110@naver.com>
2022-11-21 12:40:18 +0000

    :art: refactoring inference.py

--- code/inference.py
+++ code/inference.py
@@ -1,15 +1,16 @@
 import os
 import pickle as pickle
 
+import Model
 import numpy as np
 import pandas as pd
 import torch
 import torch.nn.functional as F
-from klue.dataloader import get_test_dataset
+from klue.dataloader import get_test_dataset , set_tokenizer
 from klue.utils import num_to_label, set_seed
 from torch.utils.data import DataLoader
 from tqdm import tqdm
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from transformers import AutoTokenizer
 
 
 # TODO : ADD TYPE HINT!
@@ -51,15 +52,26 @@ def main(conf, device):
     MODEL_NAME = f"{conf.model.model_name.replace('/','_')}_{conf.maintenance.version}"
     # load tokenizer
     tokenizer = AutoTokenizer.from_pretrained(conf.model.model_name)
+    add_token = [] #['<s>', '</s>' , '<person>' , '<location>']
+    tokenizer, new_vocab_size = set_tokenizer(tokenizer, add_token)
+    print(add_token)
+    print(new_vocab_size)
 
     ## load my model
     # TODO: model name 이 정확하게 명시된 dir 필요
-    model = AutoModelForSequenceClassification.from_pretrained(conf.path.load_model)
-    model.parameters
-    model.to(device)
-
+    
     # TODO: load dataset 통합
     test_id, test_dataset, test_label = get_test_dataset(conf.path.test_path, tokenizer)
+
+    # Model load
+    assert conf.model.model_type in ['BaseModel' ,'CustomModel'],  "model.model_type  is not ['BaseModel' , 'CustomModel']!.  please check config.yaml"
+
+    model = Model.load_model(conf.model.model_type, conf.path.load_model , new_vocab_size)
+    model = model.get_model()
+
+    print(model.config)
+    model.parameters
+    model.to(device)
 
     ## predict answer
     pred_answer, output_prob = inference(
@@ -78,6 +90,8 @@ def main(conf, device):
             "probs": output_prob,
         }
     )
+    # if not os.path.isdir(f"{conf.path.predict_dir}/{conf.model.model_name.replace('/','_')}") :
+    #     os.mkdir(f"{conf.path.predict_dir}/{conf.model.model_name.replace('/','_')}")
 
     output.to_csv(
         f"{conf.path.predict_dir}/{MODEL_NAME}.csv",
