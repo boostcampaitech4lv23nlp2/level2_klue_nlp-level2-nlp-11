4f1812f20236f42ecdac2ab3a2fa2b86dc5c2152
Sangsu <ggb04110@naver.com>
2022-11-22 04:26:37 +0000

    :art: refactoring inference.py

--- code/inference.py
+++ code/inference.py
@@ -1,13 +1,12 @@
 import os
 import pickle as pickle
 
-import Model
 import numpy as np
 import pandas as pd
 import torch
 import torch.nn.functional as F
-from klue.dataloader import set_tokenizer, load_dataloader
-
+from klue.dataloader import load_dataloader, set_tokenizer
+from klue.Model import load_model
 from klue.utils import num_to_label, set_seed
 from torch.utils.data import DataLoader
 from tqdm import tqdm
@@ -58,17 +57,25 @@ def main(conf, device):
 
     ## load my model
     # TODO: model name 이 정확하게 명시된 dir 필요
-    
+
     # TODO: load dataset 통합
-    assert conf.data.data_loader in ['BaseDataLoader' ,'CustomDataLoader'], "data.data_loader is not ['BaseDataLoader' , 'CustomDataLoader']!.  please check config.yaml"
+    assert conf.data.data_loader in [
+        "BaseDataLoader",
+        "CustomDataLoader",
+    ], "data.data_loader is not ['BaseDataLoader' , 'CustomDataLoader']!.  please check config.yaml"
 
     # load dataset
-    test_id, test_dataset, test_label = load_dataloader(conf.data.data_loader, conf.path.test_path, tokenizer).get_test_dataset()
+    test_id, test_dataset, test_label = load_dataloader(
+        conf.data.data_loader, conf.path.test_path, tokenizer
+    ).get_test_dataset()
 
     # Model load
-    assert conf.model.model_type in ['BaseModel' ,'CustomModel'],  "model.model_type  is not ['BaseModel' , 'CustomModel']!.  please check config.yaml"
+    assert conf.model.model_type in [
+        "BaseModel",
+        "CustomModel",
+    ], "model.model_type  is not ['BaseModel' , 'CustomModel']!.  please check config.yaml"
 
-    model = Model.load_model(conf.model.model_type, conf.path.load_model , new_vocab_size)
+    model = load_model(conf.model.model_type, conf.path.load_model, new_vocab_size)
     model = model.get_model()
     print(model)
     print(model.config)
